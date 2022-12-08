d5dcb16ea79d26e2af410ddd58b82389f3e21a6b
Uros Blagojevic <uros.blagojevic@antech-consulting.com>
2022-12-08 21:52:39 +0100

    Project setup

---
+++ xml-veb-servisi/src/main/java/com/xmlvebservisi/XmlVebServisiApplication.java
@@ -0,0 +1,13 @@
+package com.xmlvebservisi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlVebServisiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlVebServisiApplication.class, args);
+	}
+
+}
---
+++ xml-veb-servisi/src/test/java/com/xmlvebservisi/XmlVebServisiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmlvebservisi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlVebServisiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
